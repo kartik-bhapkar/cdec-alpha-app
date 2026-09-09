629c4e42a9b716cc04ba429fdaa80adc4f17ae00
Kartik Bhapkar | DevOps Engineer <80088899+kartik-bhapkar@users.noreply.github.com>
2026-09-10 00:21:38 +0530

    Change default AWS region from ap-south-1 to eu-west-1

--- infrastructure/frontend/variables.tf
+++ infrastructure/frontend/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for S3 and regional resources. ACM for CloudFront must be in us-east-1."
   type        = string
-  default     = "ap-south-1"
+  default     = "eu-west-1"
 }
 
 variable "environment" {
