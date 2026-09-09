ad7b04aaf80d99eb5dc26f509d1806ddf6f54d45
Kartik Bhapkar | DevOps Engineer <80088899+kartik-bhapkar@users.noreply.github.com>
2026-09-10 02:26:18 +0530

    Update backend.tf

--- infrastructure/backend/backend.tf
+++ infrastructure/backend/backend.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "cdec-alpha-terraform-state-anup"
+    bucket = "cdec-alpha-terraform-state-kartik"
     key    = "backend/terraform.tfstate"
     region = "ap-south-1"
     #profile = "terraform-sessions"
